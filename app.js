e47a1f67a5afd7659ea7cd98094f7e55a45015e8
LiamOSullivan <lmosulli@tcd.ie>
2020-02-04 16:20:57 +0000

    Setup

--- app.js
+++ app.js
@@ -37,5 +37,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render('error')
 })
+const t = new Date()
+console.log('BCD Tools started at ' + t.getHours() + ':' + t.getMinutes() + ':' + t.getSeconds())
 
 module.exports = app
