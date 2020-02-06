16359b904ff9bdb80701b8c158a3f0753a3c5c73
LiamOSullivan <lmosulli@tcd.ie>
2020-02-06 19:49:59 +0000

    Fetch nested category labels

--- public/javascripts/utils/bcd-statbank.mjs
+++ public/javascripts/utils/bcd-statbank.mjs
@@ -1,7 +1,7 @@
 import JSONstat from 'https://unpkg.com/jsonstat-toolkit@1.0.8/import.mjs'
 import { datalist } from 'https://unpkg.com/jsonstat-utils@2.5.5/export.mjs'
 
-let getTableMetadata = (tableJson) => {
+const getTableMetadata = (tableJson) => {
   // /***
   // JSON-stat Javascript Toolkit (JJK)
   // ***/
@@ -11,16 +11,34 @@ let getTableMetadata = (tableJson) => {
   const update = jsonStat.Dataset(0).updated
   const length = jsonStat.Dataset(0).value.length
   const dimensions = jsonStat.Dataset(0).id // Dimensions
+  // let categories = dimensions.forEach((dim)=>{
+  //
+  // })
+  let categories = dimensions
+    .map((dim) => jsonStat.Dataset(0).Dimension(dim).Category())
+    .map((cat) => cat.map(c => c.label))
+  console.log(categories)
+
+  // const categories = jsonStat.Dataset(0).Dimension(0).Category()
   return {
     label: label,
     source: source,
     update: update,
     valueslength: length,
     dimensions: dimensions
+    // categories: categories
   }
 }
 
 export { getTableMetadata }
+
+const filterForDublin = () => {
+  let filtered = []
+
+  return filtered
+}
+
+export { filterForDublin }
 
 // JSONstat(JSON.parse(JSON.stringify(tableJson))) // clone
   // const label = stat.Dataset(0).Dimension(0).Category('IE21').label
